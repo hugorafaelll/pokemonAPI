788a70fa49f122a7a9dcd766b0ddd781d8563845
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-11-08 23:56:02 -0300

    colocar axios

--- src/componentes/PokeList.js
+++ src/componentes/PokeList.js
@@ -4,26 +4,13 @@ import PokemonCard from "./PokemonCard.js";
 import "./PokeList.css";
 
 function PokeList() {
-  // const url = "https://pokeapi.co/api/v2/pokemon?limit=649&offset=0";
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=649&offset=0";
   const [allPokemons, setAllPokemons] = useState([]);
 
   const getAllPokemons = async () => {
     const res = await fetch(
       "https://pokeapi.co/api/v2/pokemon?limit=649&offset=0"
     );
-    const data = await res.json();
-
-    function createPokemonObject(results) {
-      results.forEach(async (pokemon) => {
-        const res = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-        );
-        const data = await res.json();
-        setAllPokemons((currentList) => [...currentList, data]);
-        await allPokemons.sort((a, b) => a.id - b.id);
-      });
-    }
-    createPokemonObject(data.results);
   };
 
   useEffect(() => {
