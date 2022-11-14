62bb78bc9c9aac621b83394726fe4cd47b3a5da3
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-11-13 23:51:31 -0300

    modalComponent

--- src/componentes/Modal.js
+++ src/componentes/Modal.js
@@ -1,7 +1,35 @@
 import React from "react";
-
-function Modal() {
-  return <div>Modal</div>;
+import pokeball from "../images/pokeball.png";
+import bg from "../images/bg";
+function Modal({
+  onclick,
+  id,
+  name,
+  image,
+  type,
+  height,
+  weight,
+  stats,
+  statsName,
+}) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        position: "fixed",
+        top: "0",
+        left: "0",
+        zIndex: "10",
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        backgroundImage: `url(${bg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    ></div>
+  );
 }
 
 export default Modal;
